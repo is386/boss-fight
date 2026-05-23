27322ec57d58393f6e70f536ed4a3c29fe41b399
is386 <20195347+is386@users.noreply.github.com>
2026-05-23 14:43:05 -0400

    disable crouch attacking

--- src/player/states/idle_attack.gd
+++ src/player/states/idle_attack.gd
@@ -11,6 +11,9 @@ func enter() -> void:
 	player.movement_state_machine.current_state.play_entry_animation()
 
 func process_input(_event: InputEvent) -> State:
+	if player.movement_component.can_crouch():
+		return
+
 	if player.movement_component.can_attack():
 		if player.movement_component.can_run():
 			return running_attack_state 
